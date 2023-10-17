40a154d65e4e47fee02f41d252b73d04519709b9
Erik Knoef <erikknoef1997@hotmail.com>
2023-10-17 17:20:27 +0100

    spacer

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,6 +3,7 @@ import styled from "styled-components/native";
 import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
@@ -79,9 +80,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <View style={{paddingLeft:16}} />
+            <Spacer variant="left.large" />
             {isOpenNow && <Open xml={open} width={20} height={20} />}
-            <View style={{paddingLeft:16}} />
+            <Spacer variant="left.large" />
             <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
